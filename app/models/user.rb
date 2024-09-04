6d71429a36de3b5f24ea0e14a99b161ea5689040
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-05 00:09:57 +0900

    エラー分が複数回出力される不具合を解消。

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   validates :family_name_kana, presence: true
   validates :birthday,         presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :family_name
     validates :first_name_kana
