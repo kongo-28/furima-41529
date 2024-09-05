7cbde24de39f49859d5c94efffc559afbfde7786
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-05 21:42:50 +0900

    imageのバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :period
   has_one_attached :image
 
+  validates :image,         presence: true
   validates :name,          presence: true
   validates :explanation,   presence: true
   validates :category_id,   numericality: { other_than: 1 , message: "can't be blank"}
