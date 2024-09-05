c8fb45a2d044786fc7cce85872680f5da532844a
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-05 19:47:42 +0900

    商品出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    Item.create(item_parameter)
+    redirect_to root_path
   end
-  
+
+  private
+
+  def item_parameter
+    params.require(:item).permit(
+    :name, :explanation, :category_id,
+    :condition_id, :fee_id, :prefecture_id,
+    :period_id, :price)
+  end
+
 
 end
