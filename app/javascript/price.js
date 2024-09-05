7ae65ee83f4e99479545abfa9ea59066383cb61a
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-06 07:19:42 +0900

    販売手数料や販売利益の計算

---
+++ app/javascript/price.js
@@ -0,0 +1,16 @@
+function price (){
+
+  // elementを取得
+  const itemPrice = document.getElementById("item-price");
+  const tax = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  // 数字を入力した際の挙動
+  itemPrice.addEventListener('keyup', function(){
+    const taxCalc = `${Math.floor(itemPrice.value*0.1)}`;
+    tax.innerHTML = `${taxCalc}`;
+    profit.innerHTML = `${itemPrice.value - taxCalc}`;
+  })
+};
+
+window.addEventListener('turbo:load', price);
