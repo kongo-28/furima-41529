c9da08f2a31e661c7f0643b53b5db0e46cbe86b0
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-08 01:13:14 +0900

    フォームの情報を保存できるよう実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,19 +7,21 @@ class OrdersController < ApplicationController
 
   def create
     @order = OrderForm.new(order_params)
+    @item = Item.find(params[:item_id])
     if @order.save
       redirect_to root_path
     else
-      render :create, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
 
   def order_params
-    params.require(:order).permit(
+    params.require(:order_form).permit(
       :post_code, :prefecture_id, :municipality, 
       :street_address, :building, :phone_number,
-      ).merge(:item_id)
+    ).merge(
+      user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/forms/order_form.rb
+++ app/forms/order_form.rb
@@ -5,7 +5,7 @@ class OrderForm
   # ゲッターとセッターを定義
   attr_accessor :post_code, :prefecture_id, :municipality, 
                 :street_address, :building, :phone_number,
-                :order, :item, :user
+                :order_id, :item_id, :user_id
 
   # バリデーション
   validates :post_code,      presence: true
@@ -15,6 +15,26 @@ class OrderForm
   validates :phone_number,   presence: true
 
   def save
-    # 処理
+    # バリデーション実行
+    return false unless valid?
+
+    ActiveRecord::Base.transaction do
+      order = Order.new(
+        user_id: user_id,
+        item_id: item_id
+      )
+      order.save
+
+      address = Address.new(
+        post_code: post_code,
+        prefecture_id: prefecture_id, 
+        municipality: municipality,
+        street_address: street_address, 
+        building: building,
+        phone_number: phone_number,
+        order_id: order.id
+      )
+      address.save
+    end
   end
 end
