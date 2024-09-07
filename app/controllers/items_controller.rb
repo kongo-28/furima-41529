1f3668c6acca0c40ea9032319bee7650dcdd1ee4
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-07 16:20:04 +0900

    商品を削除しようとすると、未ログインならログイン画面へ、他の人が出品した商品ならトップページへ遷移する機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -38,8 +38,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.delete
-    redirect_to root_path
+    if @item.user != current_user
+      redirect_to root_path
+    else
+      @item.delete
+      redirect_to root_path
+    end
   end
 
   private
