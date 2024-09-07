09230775834fe3b037408c41a41d659d27df707f
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-08 03:35:53 +0900

    HTML要素をフォームと置き換え

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,19 @@
 const pay = () => {
-  const publicKey = gon.public_key
-  const payjp = Payjp(publicKey) // PAY.JPテスト公開鍵  
-  
+  const publicKey = gon.public_key // PAY.JPテスト公開鍵 
+  const payjp = Payjp(publicKey)
+
+  // HTML要素をフォームと置き換え
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+  // HTML要素をフォームと置き換え
+
+
   const form = document.getElementById('button')
   form.addEventListener("click", (e) => {
     console.log("フォーム送信時にイベント発火")
